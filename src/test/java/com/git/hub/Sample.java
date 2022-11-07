c7e84ae7f0d55c91a1ea6af4facd58881252bdb1
EnithPradeep <enithpradeep619@gmail.com>
2022-11-07 19:25:52 +0530

    add three methods first time

---
+++ src/test/java/com/git/hub/Sample.java
@@ -0,0 +1,14 @@
+package com.git.hub;
+
+public class Sample {
+	public void tamilNadu() {
+		System.out.println("Tamil Language");
+	}
+	public void kerala() {
+		System.out.println("Malayalam Language");
+	}
+	public void maharasta() {
+		System.out.println("Marati Language");
+	}
+
+}
