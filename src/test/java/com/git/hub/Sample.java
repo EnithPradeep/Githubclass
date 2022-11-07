175f315d042ee6fe847ea01ccb49d5c5f978102a
EnithPradeep <enithpradeep619@gmail.com>
2022-11-07 19:58:23 +0530

    added three methods on national

--- src/test/java/com/git/hub/Sample.java
+++ src/test/java/com/git/hub/Sample.java
@@ -10,5 +10,14 @@ public class Sample {
 	public void maharasta() {
 		System.out.println("Marati Language");
 	}
-
+	
+	public void china() {
+		System.out.println("Chinese Language");
+	}
+	public void japan() {
+		System.out.println("Japanese Language");
+	}
+	public void londan() {
+		System.out.println("English Language");
+	}
 }
